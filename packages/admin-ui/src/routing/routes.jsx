49ae514709435798c3bdc251980df73fae56b0d3
Rafael Taranto <rafael.taranto@protonmail.com>
2025-05-16 08:15:51 +0100

    fix: machines/:id routing

--- packages/admin-ui/src/routing/routes.jsx
+++ packages/admin-ui/src/routing/routes.jsx
@@ -93,7 +93,7 @@ const Routes = () => {
           </div>
         </Transition>
       </PrivateRoute>
-      <PrivateRoute path="/machines">
+      <PrivateRoute path="/machines/:id">
         <Machines />
       </PrivateRoute>
       <PublicRoute path="/register" component={Register} />
