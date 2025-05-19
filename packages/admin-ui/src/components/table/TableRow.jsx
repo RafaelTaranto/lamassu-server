02f9a1bac5bca190c0007bb019e4d0105800c2f9
Rafael Taranto <rafael.taranto@protonmail.com>
2025-05-19 10:23:19 +0100

    fix: logs table height

--- packages/admin-ui/src/components/table/TableRow.jsx
+++ packages/admin-ui/src/components/table/TableRow.jsx
@@ -4,7 +4,8 @@ import React, { memo } from 'react'
 const TableRow = memo(
   ({ className, children, header, error, success, size = 'sm', ...props }) => {
     const classnamesObj = {
-      'p-1 h-12 bg-white': !header,
+      'p-1 bg-white': !header,
+      'h-12': !header && size !== 'sm' && size !== 'lg',
       'h-8': !header && size === 'sm',
       'h-9 font-bold text-base ': !header && size === 'lg',
       'bg-misty-rose': error,
