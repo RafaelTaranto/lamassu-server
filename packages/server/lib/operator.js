9a55922c8768e266cd3de3562d0255ec5da4cf16
siiky <github-siiky@net-c.cat>
2025-06-02 17:59:01 +0100

    fix: parameterize query

--- packages/server/lib/operator.js
+++ packages/server/lib/operator.js
@@ -2,8 +2,8 @@ const db = require('./db')
 const _ = require('lodash/fp')
 
 function getOperatorId(service) {
-  const sql = `SELECT operator_id FROM operator_ids WHERE service = '${service}'`
-  return db.oneOrNone(sql).then(_.get('operator_id'))
+  const sql = 'SELECT operator_id FROM operator_ids WHERE service = ${service}'
+  return db.oneOrNone(sql, { service }).then(_.get('operator_id'))
 }
 
 module.exports = { getOperatorId }
