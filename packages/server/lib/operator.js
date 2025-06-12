ae2bd9d9c2bffb31e665778f257fbb6d9a78c76d
siiky <github-siiky@net-c.cat>
2025-06-12 15:41:35 +0100

    feat: cache the operator ID

--- packages/server/lib/operator.js
+++ packages/server/lib/operator.js
@@ -1,8 +1,14 @@
 const db = require('./db')
 
+let operatorId = {}
+
 function getOperatorId(service) {
+  if (operatorId[service]) return Promise.resolve(operatorId[service])
+
   const sql = 'SELECT operator_id FROM operator_ids WHERE service = ${service}'
-  return db.oneOrNone(sql, { service }, ({ operator_id }) => operator_id)
+  return db.one(sql, { service }, ({ operator_id }) => {
+    return (operatorId[service] = operator_id)
+  })
 }
 
 module.exports = { getOperatorId }
